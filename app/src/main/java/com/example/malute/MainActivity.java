397171bffcc8dc0d676d438bdc98be44ce80e7ff
dreamqavk <leonovvlad098@gmail.com>
2026-04-21 18:01:34 +0300

    update: Создано окно входа и вход по пин-коду

--- app/src/main/java/com/example/malute/MainActivity.java
+++ app/src/main/java/com/example/malute/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.malute;
 
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -20,5 +22,10 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+        Button btnVk = findViewById(R.id.btnVk);
+        Drawable drawable = getResources().getDrawable(R.drawable.logo_vk);
+        drawable.setBounds(0, 0, 40, 40);
+        btnVk.setCompoundDrawables(drawable, null, null, null);
+        btnVk.setCompoundDrawablePadding(10);
     }
 }
